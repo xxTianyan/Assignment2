d2958863ec2a87b9f3ae0d2b279a125d9e0797fb
Tianyan Xu <xuty1999@outlook.com>
2025-02-23 19:00:39 -0500

    package initialize

---
+++ src/msa/__init__.py
@@ -0,0 +1,11 @@
+# src/msa/__init__.py
+from .base_classes import Node, Element, Structure
+from .base_classes import Material, Section
+
+__all__ = [
+    "Node",
+    "Element",
+    "Structure",
+    "Material",
+    "Section"
+]
